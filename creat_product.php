bbb14cbbe62402fbfcb8b152463ed8aaac0ad361
eric_tu <eric_tu@localhost.localdomain>
2017-05-12 23:54:32 +0800

    [Feature] Adding Bug Entities

---
+++ creat_product.php
@@ -0,0 +1,15 @@
+<?php
+
+//vreat_product.php
+
+require_once "bootstrap.php";
+
+$newProductName = $argv[1];
+
+$product = new Product();
+$product->setName($newProductName);
+
+$entityManager->persist($product);
+$entityManager->flush();
+
+echo "Created Product wiht ID " . $product->getId() . "\n";
---
+++ src/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+// src/Product.php
+
+// define class & table name
+/**
+ * @Entity @Table(name="products")
+ **/
+
+
+class Product
+{
+    /*The id property is defined with the id tag, this has a generator tag nested inside which defines that the primary key generation mechanism automatically uses the database platforms native id generation strategy (for example AUTO INCREMENT in the case of MySql or Sequences in the case of PostgreSql and Oracle).*/
+
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+    /** @Column(type="string") **/
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
